a41a6a9422b92304a588572b22007897a7a8a973
cvilanova76 <carolinavilanova@gmail.com>
2023-02-03 17:43:18 -0300

    atualizado até local storage

---
+++ assets/scripts/index.js
@@ -0,0 +1,46 @@
+const form =document.querySelector("#form-create-task");
+const tbodyTasks =document.querySelector("#tbody-tasks");
+
+const KEY_TASKS_LOCAL_STORAGE = "tasks"
+
+const tasks = [];
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formValues = event.target;
+
+    const { title, description } = formValues;
+
+    tasks.push({
+        title: title.value,
+        description: description.value
+    });
+    title.value = "";
+    description.value = "";
+
+    updateViewTable(tasks);
+    saveTasksLocalStorage();
+});
+
+function updateViewTable (list){
+    tbodyTasks.innerHTML = "";
+
+    list.forEach((item, index)=>{
+        const trElement = document.createElement("tr");
+
+        trElement.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${item.title}</td>
+        <td>${item.description}</td>
+        <td>ações</td>;
+        `;
+
+        tbodyTasks.appendChild(trElement);
+    });
+}
+
+function saveTasksLocalStorage(){
+    const listTasksString = JSON.stringify(tasks);
+    localStorage.setItem(KEY_TASKS_LOCAL_STORAGE, listTasksString);
+}
