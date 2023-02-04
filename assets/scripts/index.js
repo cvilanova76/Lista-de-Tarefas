ab7bc47b5aee0bdbc10b934898ecdbe6ae8f7df8
cvilanova76 <carolinavilanova@gmail.com>
2023-02-03 23:41:51 -0300

    local storage finalizado

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,46 +1,56 @@
-const form =document.querySelector("#form-create-task");
-const tbodyTasks =document.querySelector("#tbody-tasks");
+const form = document.querySelector("#form-create-task");
+const tbodyTasks = document.querySelector("#tbody-tasks");
 
-const KEY_TASKS_LOCAL_STORAGE = "tasks"
+const KEY_TASKS_LOCAL_STORAGE = "tasks";
 
-const tasks = [];
+const tasks = getTasksLocalStorage();
+
+if(tasks.length > 0){
+    updateViewTable(tasks);
+}
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const formValues = event.target;
+  const formValues = event.target;
 
-    const { title, description } = formValues;
+  const { title, description } = formValues;
 
-    tasks.push({
-        title: title.value,
-        description: description.value
-    });
-    title.value = "";
-    description.value = "";
+  tasks.push({
+    title: title.value,
+    description: description.value,
+  });
+  title.value = "";
+  description.value = "";
 
-    updateViewTable(tasks);
-    saveTasksLocalStorage();
+  updateViewTable(tasks);
+  saveTasksLocalStorage();
 });
 
-function updateViewTable (list){
-    tbodyTasks.innerHTML = "";
+function updateViewTable(list) {
+  tbodyTasks.innerHTML = "";
 
-    list.forEach((item, index)=>{
-        const trElement = document.createElement("tr");
+  list.forEach((item, index) => {
+    const trElement = document.createElement("tr");
 
-        trElement.innerHTML = `
+    trElement.innerHTML = `
         <td>${index + 1}</td>
         <td>${item.title}</td>
         <td>${item.description}</td>
         <td>ações</td>;
         `;
 
-        tbodyTasks.appendChild(trElement);
-    });
+    tbodyTasks.appendChild(trElement);
+  });
 }
 
-function saveTasksLocalStorage(){
-    const listTasksString = JSON.stringify(tasks);
-    localStorage.setItem(KEY_TASKS_LOCAL_STORAGE, listTasksString);
+function saveTasksLocalStorage() {
+  const listTasksString = JSON.stringify(tasks);
+  localStorage.setItem(KEY_TASKS_LOCAL_STORAGE, listTasksString);
+}
+
+function getTasksLocalStorage() {
+  const dataString = localStorage.getItem(KEY_TASKS_LOCAL_STORAGE);
+  const list = JSON.parse(dataString);
+  return list;
 }
